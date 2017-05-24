6e24b8d386dbeae5160944a5fb3d61e495728465
Ihor Horbachov <igorgorbachev89@gmail.com>
2017-05-25 00:36:40 +0200

    Application Form view

--- symfony/src/ApplicationBundle/Controller/DefaultController.php
+++ symfony/src/ApplicationBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace ApplicationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ApplicationBundle\Form\ApplicationType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ApplicationBundle:Default:index.html.twig');
+        $form = $this->createForm(ApplicationType::class);
+        return $this->render(
+            'ApplicationBundle:Default:index.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 }
---
+++ symfony/src/ApplicationBundle/Form/ApplicationType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ApplicationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class ApplicationType
+ * @package ApplicationBundle\Form
+ */
+class ApplicationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class);
+        $builder->add('email', EmailType::class);
+        $builder->add('file', FileType::class);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'apply';
+    }
+}
