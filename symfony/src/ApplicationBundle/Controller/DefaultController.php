e69106c83a65f75f3ca809c42362cf32571af7ac
Ihor Horbachov <igorgorbachev89@gmail.com>
2017-05-25 12:19:14 +0200

    Form proceeding base

--- symfony/src/ApplicationBundle/Controller/DefaultController.php
+++ symfony/src/ApplicationBundle/Controller/DefaultController.php
@@ -4,12 +4,33 @@ namespace ApplicationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ApplicationBundle\Form\ApplicationType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         $form = $this->createForm(ApplicationType::class);
+        return $this->render(
+            'ApplicationBundle:Default:index.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    public function createAction(Request $request)
+    {
+        $form = $this->createForm(ApplicationType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $application = $form->getData();
+            $doctrineManager = $this->getDoctrine()->getManager();
+            $doctrineManager->persist($application);
+            $doctrineManager->flush();
+        }
+
         return $this->render(
             'ApplicationBundle:Default:index.html.twig',
             [
--- symfony/src/ApplicationBundle/Form/ApplicationType.php
+++ symfony/src/ApplicationBundle/Form/ApplicationType.php
@@ -2,18 +2,24 @@
 
 namespace ApplicationBundle\Form;
 
+use ApplicationBundle\Entity\Application;
+use ApplicationBundle\Entity\ValueObject\Applicant;
+use ApplicationBundle\Entity\ValueObject\File;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\DataMapperInterface;
 
 /**
  * Class ApplicationType
  * @package ApplicationBundle\Form
  */
-class ApplicationType extends AbstractType
+class ApplicationType extends AbstractType implements DataMapperInterface
 {
     /**
      * @param FormBuilderInterface $builder
@@ -24,6 +30,10 @@ class ApplicationType extends AbstractType
         $builder->add('name', TextType::class);
         $builder->add('email', EmailType::class);
         $builder->add('file', FileType::class);
+        $builder->add('apply', SubmitType::class, array(
+            'attr' => array('class' => 'save'),
+        ));
+        $builder->setDataMapper($this);
     }
 
     /**
@@ -31,7 +41,38 @@ class ApplicationType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setDefaults(array(
+            'data_class' => Application::class,
+            'empty_data' => function (FormInterface $form) {
+                return new Application(
+                    new Applicant(
+                        $form->get('name')->getData(),
+                        $form->get('email')->getData()
+                    ),
+                    new File($form->get('file')->getName())
+                );
+            },
+        ));
+    }
 
+    public function mapDataToForms($data, $form)
+    {
+        $form = iterator_to_array($form);
+        $form['name']->setData($data ? $data->getApplicant()->getName() : null);
+        $form['email']->setData($data ? $data->getApplicant()->getEmail() : null);
+        $form['file']->setData($data ? $$data->getFile()->getName() :null);
+    }
+
+    public function mapFormsToData($form, &$data)
+    {
+        $form = iterator_to_array($form);
+        $data = new Application(
+            new Applicant(
+                $form['name']->getData(),
+                $form['email']->getData()
+            ),
+            new File($form['file']->getData())
+        );
     }
 
     /**
