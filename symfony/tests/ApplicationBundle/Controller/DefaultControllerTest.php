a1eab9afac823db4e73300f9cd0bda1898fbef1d
Ihor Horbachov <igorgorbachev89@gmail.com>
2017-05-26 11:24:25 +0200

    DefaultController functional test p1

--- symfony/tests/ApplicationBundle/Controller/DefaultControllerTest.php
+++ symfony/tests/ApplicationBundle/Controller/DefaultControllerTest.php
@@ -3,6 +3,10 @@
 namespace ApplicationBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use ApplicationBundle\Entity\Application;
+use ApplicationBundle\Form\ApplicationType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -10,6 +14,57 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+
+    public function testCreateSuccess()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $file = new UploadedFile(
+            dirname(dirname(__FILE__)) . '/Fixtures/FileUploadTest.txt',
+            'FileUploadTest.txt',
+            'text/plain',
+            24
+        );
+
+        $form = $crawler->selectButton('Apply')->form();
+        $form->setValues(
+            [
+                'apply[name]' => 'name_' . time(),
+                'apply[email]' => time() . '@gmail.com',
+                'apply[file]'  => $file,
+            ]
+        );
+
+        $crawler = $client->submit($form);
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateFail()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $file = new UploadedFile(
+            dirname(dirname(__FILE__)) . '/Fixtures/FileUploadTest.txt',
+            'FileUploadTest.txt',
+            'text/plain',
+            24
+        );
+
+        $form = $crawler->selectButton('Apply')->form();
+        $form->setValues(
+            [
+                'apply[name]' => 'name_' . time(),
+                'apply[email]' => time() . '',
+                'apply[file]'  => $file,
+            ]
+        );
+
+        $crawler = $client->submit($form);
+        $this->assertEquals(Response::HTTP_PRECONDITION_FAILED, $client->getResponse()->getStatusCode());
     }
 }
--- symfony/tests/ApplicationBundle/Entity/ApplicationTest.php
+++ symfony/tests/ApplicationBundle/Entity/ApplicationTest.php
@@ -5,6 +5,7 @@ namespace ApplicationBundle\Tests\Entity;
 use ApplicationBundle\Entity\Application;
 use ApplicationBundle\Entity\ValueObject\Applicant;
 use ApplicationBundle\Entity\ValueObject\File;
+use ReflectionClass;
 
 /**
  * Class BlogTest
@@ -16,13 +17,50 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
      * Test constructor and getters
      */
     public function testConstructor()
-     {
-         $applicant = new Applicant('email', 'name');
-         $file = new File('filename');
+    {
+        $applicant = new Applicant('email', 'name');
+        $file = new File('filename');
 
-         $application = new Application($applicant, $file);
+        $application = new Application($applicant, $file);
 
-         $this->assertEquals($applicant, $application->getApplicant());
-         $this->assertEquals($file, $application->getFile());
-     }
+        $this->assertEquals($applicant, $application->getApplicant());
+        $this->assertEquals($file, $application->getFile());
+    }
+
+    public function testGetId()
+    {
+        $testId = 55;
+
+        $applicant = new Applicant('email', 'name');
+        $file = new File('filename');
+
+        $application = new Application($applicant, $file);
+        $class = new ReflectionClass('ApplicationBundle\Entity\Application');
+
+        $property = $class->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($application, $testId);
+
+        $this->assertEquals($testId, $application->getId());
+    }
+
+    public function testGetCreatedAt()
+    {
+        $created_at = new \DateTime();
+
+        $applicant = new Applicant('email', 'name');
+        $file = new File('filename');
+
+        $application = new Application($applicant, $file);
+        $class = new ReflectionClass('ApplicationBundle\Entity\Application');
+
+        $property = $class->getProperty('created_at');
+        $property->setAccessible(true);
+        $property->setValue($application, $created_at);
+
+        $this->assertEquals(
+            date_format($created_at, 'm-d H:i:s'),
+            $application->getCreatedAt()
+        );
+    }
 }
